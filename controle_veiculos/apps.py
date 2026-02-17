c095cf5b4e0d0f42c21bc0a23152de96869803fa
mtslopes13 <mateus.lopes13@unifesp.br>
2026-02-17 19:14:26 -0300

    initial commit

---
+++ controle_veiculos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ControleVeiculosConfig(AppConfig):
+    name = 'controle_veiculos'
---
+++ controle_veiculos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=128)
