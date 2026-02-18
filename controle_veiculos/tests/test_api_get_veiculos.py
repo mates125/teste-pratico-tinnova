cadfdf39201a022e9dd7b1ca39e875279be0bd70
mtslopes13 <mateus.lopes13@unifesp.br>
2026-02-17 21:30:43 -0300

    feat: Define método GET para Veiculos, teste get veiculos retorna apenas ativos

--- controle_veiculos/tests/test_api_get_veiculos.py
+++ controle_veiculos/tests/test_api_get_veiculos.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from controle_veiculos.models import Veiculo
 from django.contrib.auth.models import User
 
 @pytest.mark.django_db
@@ -17,3 +18,33 @@ def test_get_veiculos_com_user_autenticado_retorna_200(client):
     response = client.get(url)
 
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_get_veiculos_retorna_apenas_ativos(client):
+    user = User.objects.create_user(username="user", password="123")
+    client.force_login(user)
+
+    Veiculo.objects.create(
+        placa="AAA1111",
+        marca="Ford",
+        modelo="Ka",
+        ano=2020,
+        cor="Preto",
+        preco_usd=10000,
+        ativo=True
+    )
+
+    Veiculo.objects.create(
+        placa="BBB2222",
+        marca="Fiat",
+        modelo="Uno",
+        ano=2019,
+        cor="Branco",
+        preco_usd=9000,
+        ativo=False
+    )
+
+    url = reverse("veiculos-list")
+    response = client.get(url)
+
+    assert len(response.json()) == 1
--- controle_veiculos/views.py
+++ controle_veiculos/views.py
@@ -1,9 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from controle_veiculos.models import Veiculo
 
 class VeiculoListView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response([])
+        veiculos = Veiculo.objects.ativos()
+        data = [
+            {
+                "placa": v.placa,
+                "marca": v.marca,
+                "modelo": v.modelo,
+                "ano": v.ano,
+                "cor": v.cor,
+                "preco_usd": float(v.preco_usd),
+            }
+            for v in veiculos
+        ]
+        return Response(data)
